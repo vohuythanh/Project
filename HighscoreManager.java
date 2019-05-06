7192a48b5245cd99bf5d63cdcc2b3236b1a53437
vohuythanh <49562053+vohuythanh@users.noreply.github.com>
2019-05-06 09:06:20 +0700

    Add files via upload

---
+++ HighscoreManager.java
@@ -0,0 +1,98 @@
+package game_project;
+
+import java.util.*;
+import java.io.*;
+
+public class HighscoreManager {
+    
+    private ArrayList<Score> scores;
+
+    private static final String HIGHSCORE_FILE = "scores.dat";
+
+    //Initialising an in and outputStream for working with the file
+    ObjectOutputStream outputStream = null;
+    ObjectInputStream inputStream = null;
+
+    public HighscoreManager() {
+        
+        scores = new ArrayList<Score>();
+    }
+    public ArrayList<Score> getScores() {
+        loadScoreFile();
+        sort();
+        return scores;
+    }
+    private void sort() {
+        ScoreComparator comparator = new ScoreComparator();
+        Collections.sort(scores, comparator);
+}
+    public void addScore(String name, int score) {
+        loadScoreFile();
+        scores.add(new Score(name, score));
+        updateScoreFile();
+    }
+    public void loadScoreFile(){
+        try {
+            inputStream = new ObjectInputStream(new FileInputStream(HIGHSCORE_FILE));
+            scores = (ArrayList<Score>) inputStream.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("[Laad] FNF Error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("[Laad] IO Error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("[Laad] CNF Error: " + e.getMessage());
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.flush();
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                System.out.println("[Laad] IO Error: " + e.getMessage());
+            }
+        }
+}
+    public void updateScoreFile() {
+        try {
+            outputStream = new ObjectOutputStream(new FileOutputStream(HIGHSCORE_FILE));
+            outputStream.writeObject(scores);
+        } catch (FileNotFoundException e) {
+            System.out.println("[Update] FNF Error: " + e.getMessage() + ",the program will try and make a new file");
+        } catch (IOException e) {
+            System.out.println("[Update] IO Error: " + e.getMessage());
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.flush();
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                System.out.println("[Update] Error: " + e.getMessage());
+            }
+        }
+}
+public String getHighscoreString() {
+        String highscoreString = "";
+	final int max = 10;
+
+        ArrayList<Score> scores;
+        scores = getScores();
+
+        int i = 0;
+        int x = scores.size();
+        if (x > max) {
+            x = max;
+        }
+        while (i < x) {
+            highscoreString += (i + 1) + ".\t" + scores.get(i).getNaam() + "          " + scores.get(i).getScore() + "\n";
+            i++;
+        }
+        return highscoreString;
+}
+public int getHighScore()
+{
+        ArrayList<Score> scores;
+        scores = getScores();
+        return scores.get(0).getScore();
+}
+}
