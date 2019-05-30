749d32e8ad833075cfd43e2f6279766883415d25
vohuythanh <49562053+vohuythanh@users.noreply.github.com>
2019-05-30 23:19:37 +0700

    Add files via upload

--- Score.java
+++ Score.java
@@ -1,21 +1,21 @@
+package game_project;
+
 import java.io.Serializable;
 
-public class Score implements Serializable {
-	
-	private int score;
-	private String name;
-	
-	public int getScore() {
-		return score;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public Score(String name, int score) {
-		this.score = score;
-		this.name = name;
-	}
+public class Score  implements Serializable {
+    private int score;
+    private String naam;
 
+    public int getScore() {
+        return score;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public Score(String naam, int score) {
+        this.score = score;
+        this.naam = naam;
+    }
 }
